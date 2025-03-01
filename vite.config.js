2ccce3517469bc5a07473365e9ab1491ae3c6f4f
yangxiaoyi8242 <yangyi8242@126.com>
2025-03-02 00:40:27 +0800

    更新部署配置和静态资源路径

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/expense-control-system/',  // 设置基础路径为仓库名
+  base: '/travell.github.io/',  // 设置基础路径为GitHub Pages仓库名
 })
